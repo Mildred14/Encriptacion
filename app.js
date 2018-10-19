3d52a87dd8506312325099014dc665c81eb26e78
Mildred Silva <mildred_natali@hotmail.com>
2018-10-18 20:26:46 -0500

    Encriptacion

---
+++ app.js
@@ -0,0 +1,34 @@
+const abecedario=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","u","v","w","x","y","z"];
+let mensajeCifrado="";
+let mensajeTraducido="";
+function leerMensaje(){
+    var mensaje=document.getElementById("mensaje").value.toLowerCase();
+limpiar()
+for(let i=0; i<mensaje.length;i++){
+    for(let j=0; j<abecedario.length; j++){
+       if (mensaje.charAt(i)==abecedario[j]){
+        mensajeCifrado += abecedario[j+1];
+       }
+    }
+    document.getElementById("mensajeConvertido").value=mensajeCifrado;
+
+}
+
+}
+function traducirMensaje(){
+    var mensaje=document.getElementById("mensaje").value.toLowerCase();
+limpiar()
+for(let i=0; i<mensaje.length;i++){
+    for(let j=0; j<abecedario.length; j++){
+       if (mensaje.charAt(i)==abecedario[j]){
+        mensajeTraducido += abecedario[j-1];
+       }
+    }
+}
+document.getElementById("mensajeConvertido").value=mensajeTraducido;
+
+}
+function limpiar(){
+    document.getElementById("mensajeConvertido").value=" ";
+
+}
